007620fc4ec3a6d757f2dc8f5e19216510480805
BadriKumar2000 <g.badrikumar2000@gmail.com>
2023-09-19 17:05:35 +0530

    adding key prop to the UserProfile Component

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import "./App.css"
 
 const userDetailsList = [
   {
+    id: 1,
   imageUrl: "https://assets.ccbp.in/frontend/react-js/esther-howard-img.png",
   name: "Esther Howard",
   role: "Software Engineer"
 },
 {
+  id:2,
   imageUrl: "https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png",
   name: "Floyd Miles",
   role: "Software Engineer"
 },
 {
+  id:3,
   imageUrl: "https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png",
   name: "Jacob Jones",
   role: "Software Engineer"
 },
 {
+  id:4,
   imageUrl: "https://assets.ccbp.in/frontend/react-js/devon-lane-img.png",
   name: "Devon Lane",
   role: "Software Engineer"
@@ -27,12 +31,9 @@ const userDetailsList = [
 
 const App = ()=><div className="list-container">
   <h1 className="title">User Profile</h1>
-  <ul> 
     {
-      userDetailsList.map((eachObject)=><UserProfile userDetails={eachObject}/>)
-    }
-  </ul>
-  
+      userDetailsList.map((eachObject)=><UserProfile userDetails={eachObject} key={eachObject.id}/>)
+    } 
 </div>
 
 export default App; 
