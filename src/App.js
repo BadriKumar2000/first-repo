9589942f64888972461f388ca58ee439c3239756
BadriKumar2000 <g.badrikumar2000@gmail.com>
2023-09-20 17:45:30 +0530

    added small Para

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
 import UserProfile from "./Components/UserProfile";
-import "./App.css"
+import "./App.css";
 
 const userDetailsList = [
   {
     id: 1,
-  imageUrl: "https://assets.ccbp.in/frontend/react-js/esther-howard-img.png",
-  name: "Esther Howard",
-  role: "Software Engineer"
-},
-{
-  id:2,
-  imageUrl: "https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png",
-  name: "Floyd Miles",
-  role: "Software Engineer"
-},
-{
-  id:3,
-  imageUrl: "https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png",
-  name: "Jacob Jones",
-  role: "Software Engineer"
-},
-{
-  id:4,
-  imageUrl: "https://assets.ccbp.in/frontend/react-js/devon-lane-img.png",
-  name: "Devon Lane",
-  role: "Software Engineer"
-},
-]
+    imageUrl: "https://assets.ccbp.in/frontend/react-js/esther-howard-img.png",
+    name: "Esther Howard",
+    role: "Software Engineer",
+  },
+  {
+    id: 2,
+    imageUrl: "https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png",
+    name: "Floyd Miles",
+    role: "Software Engineer",
+  },
+  {
+    id: 3,
+    imageUrl: "https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png",
+    name: "Jacob Jones",
+    role: "Software Engineer",
+  },
+  {
+    id: 4,
+    imageUrl: "https://assets.ccbp.in/frontend/react-js/devon-lane-img.png",
+    name: "Devon Lane",
+    role: "Software Engineer",
+  },
+];
 
+const App = () => (
+  <div className="list-container">
+    <h1 className="title">User Profile</h1>
+    <p>Adding Users Search Box</p>
+    {userDetailsList.map((eachObject) => (
+      <UserProfile userDetails={eachObject} key={eachObject.id} />
+    ))}
+  </div>
+);
 
-const App = ()=><div className="list-container">
-  <h1 className="title">User Profile</h1>
-    {
-      userDetailsList.map((eachObject)=><UserProfile userDetails={eachObject} key={eachObject.id}/>)
-    } 
-</div>
-
-export default App; 
+export default App;
